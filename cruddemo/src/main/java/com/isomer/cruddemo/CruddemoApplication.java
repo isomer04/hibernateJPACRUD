bb32e530fb71cdcc2a690da516fd16b80e7ed6fe
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-23 22:09:44 -0400

    Added a read method and findByID

--- cruddemo/src/main/java/com/isomer/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/isomer/cruddemo/CruddemoApplication.java
@@ -1,6 +1,7 @@
 package com.isomer.cruddemo;
 
 import com.isomer.cruddemo.dao.StudentDAO;
+import com.isomer.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +17,61 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
+
+			// createMultipleStudents(studentDAO);
+
+			readStudent(studentDAO);
 		};
 	}
 
-	private void createStudent(StudentDAO studentDAO) {
+	private void readStudent(StudentDAO studentDAO) {
 
+		// create the student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Boo" , "Din", "james@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+		Student myStudent  = studentDAO.findById(theId);
+
+		// retrieve student based on the id: primary id
+		System.out.println("Retrieving student with id. Generated id: " + theId);
+
+		System.out.println("Found the student: " + myStudent);
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating 3 students object...");
+		Student tempStudent1 = new Student("James" , "Din", "james@gmail.com");
+		Student tempStudent2 = new Student("Jack" , "Din", "james@gmail.com");
+		Student tempStudent3 = new Student("Yvona" , "Din", "james@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("James" , "Din", "james@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
--- cruddemo/src/main/java/com/isomer/cruddemo/dao/StudentDAO.java
+++ cruddemo/src/main/java/com/isomer/cruddemo/dao/StudentDAO.java
@@ -4,4 +4,7 @@ import com.isomer.cruddemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student theStudent);
+
+    Student findById(Integer id);
+
 }
